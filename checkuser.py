4f9a4631ee7ebf40570bff8611c99f5ccf245d63
Charlie-Toro <charles_toro@mail.com>
2017-02-14 00:43:43 -0500

    checks usernames for availability in list

--- checkuser.py
+++ checkuser.py
@@ -2,15 +2,15 @@
 #Caleb Bell
 #Simulates website checking for unique user names
 
-current_users = ['kmaloney','clbell4','zilke','vania','Ctopping','mmires']
+current_users = ['kmaloney','clbell4','zilke','vania','Ctopping','mmIreS']
 new_users = ['ctopping','mmires','ecabellon','njacobs2','bflynn','mcavett']
 
-for current_user in current_users:
-    current_user.lower()
-    
-    for user in new_users:
-        user.lower()
-        if user in current_users:
-            print('USERNAME IN USE!PLEASE CHOOSE ANOTHER!')
-        else:
-            print(user + " IS AVAILABLE!")
+
+new_user = [new_user.lower() for new_user in new_users]
+current_user = [current_user.lower() for current_user in current_users]
+
+for user in new_user:
+    if user in current_user:
+        print("USERNAME IS ALREADY TAKEN!")
+    else:
+        print(user + ' IS AVAILABLE!')
