084bf2b4277801b0c74a1c10dbb4b51047c3deeb
Charlie-Toro <charles_toro@mail.com>
2017-02-14 01:52:57 -0500

    checks usernames for availability in list

---
+++ ordinal.py
@@ -0,0 +1,19 @@
+#Ordinal Number
+#Caleb Bell
+#Prints ordinal numbers
+
+numbers = [num for num in range(1,10)]
+
+print(numbers)
+
+
+
+for number in numbers:
+    if number == 1:
+        print(str(number) + 'st')
+    elif number == 2:
+        print(str(number) + 'nd')
+    elif number == 3:
+        print (str(number) + 'rd')
+    else:
+        print(str(number) + 'th')
