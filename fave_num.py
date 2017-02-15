77d084e298bc00520f1af2ed3595fccbfa773835
Charlie-Toro <charles_toro@mail.com>
2017-02-14 22:41:17 -0500

    Exercise from Python crash course practicing dictionaries

---
+++ fave_num.py
@@ -0,0 +1,17 @@
+#fave num
+#Caleb Bell
+#uses a dictionary to store individuals' favorite numbers
+
+fave_nums = {
+    'James': 345,
+    'Alex': 23,
+    'Chris': 123,
+    'Jenna': 2,
+    'Felicia': 69,
+}
+
+print(fave_nums['James'])
+print(fave_nums['Alex'])
+print(fave_nums['Chris'])
+print(fave_nums['Jenna'])
+print(fave_nums['Felicia'])
