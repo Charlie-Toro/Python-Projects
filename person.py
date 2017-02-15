a833cd089bf4d7679f95590340c61a74a623fd7d
Charlie-Toro <charles_toro@mail.com>
2017-02-14 22:31:14 -0500

    Exercise from Python crash course practicing dictionaries

---
+++ person.py
@@ -0,0 +1,16 @@
+#Person
+#Caleb Bell
+#Uses a dictionary to store information about a person
+
+person = {
+    'first_name': "Joe",
+    'last_name':"Johnson",
+    'age': 35,
+    'city': 'Baltimore',
+    }
+
+print(person['first_name'])
+print(person['last_name'])
+print(person['age'])
+print(person['city'])
+
