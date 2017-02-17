69bc33fdf0e2036c465372f1b40f740051017589
Charlie-Toro <charles_toro@mail.com>
2017-02-16 22:42:55 -0500

    Exercise from Python crash course practicing dictionaries

--- fave_num.py
+++ fave_num.py
@@ -3,15 +3,14 @@
 #uses a dictionary to store individuals' favorite numbers
 
 fave_nums = {
-    'James': 345,
-    'Alex': 23,
-    'Chris': 123,
-    'Jenna': 2,
-    'Felicia': 69,
+    'James': [345, 234, 2],
+    'Alex': [23, 12, 32],
+    'Chris': [123, 23, 12],
+    'Jenna': [2, 12, 12],
+    'Felicia': [69, 2, 43],
 }
 
-print(fave_nums['James'])
-print(fave_nums['Alex'])
-print(fave_nums['Chris'])
-print(fave_nums['Jenna'])
-print(fave_nums['Felicia'])
+for person, nums in sorted(fave_nums.items()):
+    print(person.title() + " 's favorite numbers are: ")
+    for num in nums:
+        print(num)
