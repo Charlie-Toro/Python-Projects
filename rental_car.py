3b77bf7b22371a2be8fb8a3d27a62606461822d8
Charlie-Toro <charles_toro@mail.com>
2017-02-17 22:44:31 -0500

    Exercise from Python crash course practicing user input

---
+++ rental_car.py
@@ -0,0 +1,6 @@
+#rental car
+#Caleb Bell
+#Asks user what type of rental car they would like, demonstrates accepting input
+
+rental_car = input("What kind of rental car would you like?: ")
+print(" I will see if I can find you a " + rental_car)
