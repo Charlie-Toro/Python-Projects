8bae67d8118fbd5e06ee21d79fcf5f7af0da047b
Charlie-Toro <charles_toro@mail.com>
2017-02-17 22:50:35 -0500

    Exercise from Python crash course practicing user input

---
+++ rest_seating.py
@@ -0,0 +1,9 @@
+#Restaurant Seating
+#Caleb Bell
+# Ask user for how many people to seat at a table
+
+seat = input("How many people are in your dinner party?: ")
+seat = int(seat)
+
+if seat > 8:
+    print("You will have to wait for seating.")
