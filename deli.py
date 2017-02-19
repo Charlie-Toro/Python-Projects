efc1aece403d5087a318e0c60a348890d6493e20
Charlie-Toro <charles_toro@mail.com>
2017-02-18 20:28:41 -0500

    Exercise from Python crash course practicing user input and while loops

--- deli.py
+++ deli.py
@@ -2,15 +2,21 @@
 # Caleb Bell
 # Takes order of sandwiches and stores them in list
 
-sandwich_orders = ['PB &J', 'Reuben', 'Ham & Cheese', 'Turkey', 'Tuna']
+sandwich_orders = ['PB &J', 'Reuben', 'Ham & Cheese', 'Turkey', 'Tuna', 'Pastrami', 'Pastrami', 'Pastrami']
 finished_sandwiches = []
 
+print("Unfortunately, the deli has run out of pastrami!")
+
 while sandwich_orders:
+
+    while 'Pastrami' in sandwich_orders:
+        sandwich_orders.remove('Pastrami')
+
     made_sandwich = sandwich_orders.pop()
-    print('Your ' + made_sandwich + ' has been completed')
+    print('Your ' + made_sandwich + ' has been completed\n')
     finished_sandwiches.append(made_sandwich)
 
 for sandwich in finished_sandwiches:
-    print(sandwich)
+    print(sandwich + '\n')
 
 
