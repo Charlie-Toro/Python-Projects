ba2cea08248d8840d0b0de3c9071d299697281ca
Charlie-Toro <charles_toro@mail.com>
2017-03-28 23:50:02 -0400

    practices exceptions

--- addition.py
+++ addition.py
@@ -3,14 +3,31 @@
 # Prompts for 2 numbers and catches error if text provided
 
 
-def addition(num1, num2):
+def addition(nums=[]):
+    s = 0
+    for num in nums:
+        s += num
+        print(s)
+
+
+num_list = list()
+
+while True:
     try:
-        print(int(num1) + int(num2))
+        number = input("Please enter a number(Enter Done to quit): ")
+        if number.lower() == 'done':
+            break
+        n = int(number)
+        num_list.append(n)
+        addition(num_list)
     except ValueError:
-        print("Sorry, you provided a non-numeric input")
+        pass
 
 
-number1 = input('Please enter first number: ')
-number2 = input('Please enter second number: ')
 
-addition(number1, number2)
+
+
+
+
+
+
