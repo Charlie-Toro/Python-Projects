f7b74950445db832d664d24c90610cde880ac379
Charlie-Toro <charles_toro@mail.com>
2017-06-04 21:40:37 -0400

    practices exceptions

---
+++ strong_password.py
@@ -0,0 +1,33 @@
+# Strong Password Generator
+# Caleb Bell
+# Generates password based on criteria
+# The flag is your password.
+# Here are some guidelines for choosing a good password:
+# Your password must be at least twenty characters long.
+# Your password must be less than twenty-one characters long.
+# Your password must contain only numbers and uppercase letters.
+# Your password must contain at least ten digits.
+# Your password must start with the Unicode equivalent (not include U+) of the emoji pair victory hand no entry sign X
+# You password must contain at least seven uppercase 'C' characters.
+# Your password must not repeat any character more than once, with the exception of uppercase 'C'.
+# The last five characters of your password must be numbers.
+# The SHA-256 hex hash of your password must match the following:
+#  2fcc64cad25d0450497348cc3810685dde660c8fb29a9a153804fa4a5b0d395
+# **********************************************************************************************************************
+
+import hashlib
+import string
+import random
+
+
+def password_gen(length):
+    password = '270C'
+    return password.join()
+
+
+
+
+
+
+
+
